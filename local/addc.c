43a970f8a7e977bcbfff52d1f5b0eec5912d14c1
tannal <tannal2409@gmail.com>
2024-02-29 22:07:39 +0800

    update

--- local/addc.c
+++ local/addc.c
@@ -1,8 +1,20 @@
-#include <stdio.h>
 
-int main(void) {
-    int X = 10;
-    int Y = 32;
-    printf("Hello, world addc %d\n", X + Y);
-    return 0;
+double pi() {
+    int num_terms = 1000000;  // Number of terms to use in the series
+    double pi_approx = 0.0;
+    int sign = 1;  // To alternate between adding and subtracting terms
+    double divisor = 1.0;
+
+    for (int i = 0; i < num_terms; i++) {
+        pi_approx += sign * (1.0 / divisor);
+        sign = -sign;  // Change sign for the next term
+        divisor += 2.0;  // Increment the divisor by 2 for the next term
+    }
+
+    // Multiply by 4 to get the value of π
+    pi_approx *= 4.0;
+
+    // printf("Approximation of π using %d terms: %lf\n", num_terms, pi_approx);
+
+    return pi_approx;
 }
---
+++ local/ast.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int a = 5;
+    int b = 10;
+    int sum = a + b;
+
+    printf("The sum is %d\n", sum);
+
+    return 0;
+}
